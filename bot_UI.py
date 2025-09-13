cb67b5134b41a2ae0e1bb0b0fa563cbf38eef5cc
Gryffin T <Gryffintubon@gmail.com>
2025-09-12 23:47:02 -0500

    Update bot_UI.py

--- bot_UI.py
+++ bot_UI.py
@@ -21,15 +21,8 @@ if prompt := st.chat_input("Ask me anything"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ], 
-            stream=True
-        )
-        response = st.write_stream(stream)
+        classifications = pipeline.predict(statement)
+        response = f"The classifications are: {classifications}."
     st.session_state.messages.append({"role": "assistant", "content": response})
       
       
