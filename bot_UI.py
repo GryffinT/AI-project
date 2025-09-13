08fd7d658be9244a653b5ccc34cd0affecaf7f9f
Gryffin T <Gryffintubon@gmail.com>
2025-09-12 23:23:26 -0500

    Update bot_UI.py

--- bot_UI.py
+++ bot_UI.py
@@ -3,12 +3,14 @@ import classification_data
 import streamlit as st
 import random
 
+# Initialize chat history
 if "messages" not in st.session_state:
-  st.session_state.messages = []
+    st.session_state.messages = []
 
-for message in st.session_state.message:
-  with st.chat_message(message=["role"]):
-    st.markdown(message["content"])
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
 st.title("Laurant.CA")
 st.write ("Logistic-Regression Transformer Classifcation Algorithm")
