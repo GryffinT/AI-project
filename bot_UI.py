8b1d811788ce2e816ccbaa0b411865bf3413acb4
Gryffin T <Gryffintubon@gmail.com>
2025-09-13 00:28:54 -0500

    Update bot_UI.py

--- bot_UI.py
+++ bot_UI.py
@@ -2,37 +2,6 @@ import Main_classification
 import classification_data
 import streamlit as st
 import random
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
-import time
-# Now you can safely use st.session_state.le_p_classes, etc.
-time.sleep(60)
-with st.sidebar:
-
-    fig, axes = plt.subplots(1, 2, figsize=(14,6))
-
-    # Primary classifier scatter
-    axes[0].scatter(st.session_state.X_pca[:,0], st.session_state.X_pca[:,1],
-                    c=st.session_state.p_labels_encoded, cmap='tab10', alpha=0.7, edgecolor='k')
-    axes[0].set_title("Primary Classifier")
-    axes[0].legend(handles=[
-        Line2D([0], [0], marker='o', color='w', label=cls,
-               markerfacecolor=plt.cm.tab10(i / len(st.session_state.le_p_classes)), markersize=10)
-        for i, cls in enumerate(st.session_state.le_p_classes)
-    ], title="Classes")
-
-    # Secondary classifier scatter
-    axes[1].scatter(st.session_state.X_pca[:,0], st.session_state.X_pca[:,1],
-                    c=st.session_state.s_labels_encoded, cmap='tab20', alpha=0.7, edgecolor='k')
-    axes[1].set_title("Secondary Classifier")
-    axes[1].legend(handles=[
-        Line2D([0], [0], marker='o', color='w', label=cls,
-               markerfacecolor=plt.cm.tab20(i / len(st.session_state.le_s_classes)), markersize=10)
-        for i, cls in enumerate(st.session_state.le_s_classes)
-    ], title="Classes")
-
-    plt.tight_layout()
-    st.pyplot(fig)
     
 st.title("Laurant.CA")
 st.write ("Logistic-Regression Transformer Classifcation Algorithm")
