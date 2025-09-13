195e6053065d39533f5cad80397e809b32e77d29
Gryffin T <Gryffintubon@gmail.com>
2025-09-12 23:19:09 -0500

    Update bot_UI.py

--- bot_UI.py
+++ bot_UI.py
@@ -1,1 +1,21 @@
+import Main_classification
+import classification_data
+import streamlit as st
+import random
+
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+
+for message in st.session_state.message:
+  with st.chat_message(message=["role"]):
+    st.markdown(message["content"])
+
+st.title("Laurant.CA")
+st.write ("Logistic-Regression Transformer Classifcation Algorithm")
+
+random_message = data.keys()(math.rand(0,5))
+if prompt := st.chat_input(random_message):
+  with st.chat_message("user"):
+    st.markdown(prompt)
+  st.session_state.messages.append("role": "user", "content": prompt)
 
